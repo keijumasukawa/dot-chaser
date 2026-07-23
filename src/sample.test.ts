6804c5f8a29e28cdc9b939d55d921cb4fc6b0cd5
Keiju Masukawa <keiju.masukawa@gmail.com>
2026-07-23 19:43:01 +0900

    chore: ESLint + Prettier を導入

--- src/sample.test.ts
+++ src/sample.test.ts
@@ -1,7 +1,7 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it } from "vitest";
 
-describe('テスト環境の動作確認', () => {
-  it('Vitest が実行できる', () => {
+describe("テスト環境の動作確認", () => {
+  it("Vitest が実行できる", () => {
     expect(1 + 1).toBe(2);
   });
 });
